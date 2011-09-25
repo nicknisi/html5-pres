30b037dc6bae1e52ff99000cae143da23608b02d
Nick Nisi <nicknisi@gmail.com>
2011-09-25 13:17:00 -0500

    initial presentation working

--- js/prez.js
+++ js/prez.js
@@ -5,4 +5,59 @@
  * Written for NECFUG HTML 5 Talk
  **/
 
-var prez = (function ($, window, undefined) {})(jQuery, window);
+var prez = {
+	numSlides : 0,
+	currSlide : 0,
+	init : function () {
+		$('section.slide:first').show();
+		this.currSlide = 0;
+	},
+	next : function () {
+		if (this.currSlide < this.numSlides - 1) {
+			++this.currSlide;
+			this.showSlide();
+		}
+	},
+	previous : function () {
+		if (this.currSlide - 1 >= 0) {
+			--this.currSlide;
+			this.showSlide();
+		}
+	},
+	showSlide : function () {
+		$('section.slide').hide();
+		$('section.slide:eq(' + this.currSlide + ')').show();
+	}
+};
+
+jQuery(function () {
+	prez.numSlides = $('section.slide').length;
+	console.log('prez.numSlides = ' + prez.numSlides);
+	$(document)
+		.bind('mouseover.prez', function (e) {
+			if (e.pageY < 150) {
+				$('.deck header').fadeIn('fast');
+			}
+			else {
+				$('.deck header').fadeOut('fast');
+			}
+		})
+		.bind('keypress.prez', function (e) {
+			switch (e.which) {
+				case 37: // left
+				case 38: // up
+				case 112: // p
+				prez.previous();
+				break;
+				case 39: // right
+				case 40: // down
+				case 110: // n
+				case 32: // spacebar
+				case 13: // enter
+				prez.next();
+				break;
+			}
+		});
+	$('section.slide').hide();
+	prez.init();
+});
