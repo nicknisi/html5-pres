925d1c57b374cad5d979f8b12cddf5710a529694
Nick Nisi <nicknisi@gmail.com>
2011-09-25 11:09:36 -0500

    initial commit

---
+++ js/prez.js
@@ -0,0 +1,8 @@
+/**
+ * Presentation.js
+ * Simple JavaScript Presentation Framework
+ * Written by Nick Nisi
+ * Written for NECFUG HTML 5 Talk
+ **/
+
+var prez = (function ($, window, undefined) {})(jQuery, window);
